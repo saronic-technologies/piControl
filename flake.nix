7ed73cbf10ea66cdfb9856ec5bed552e79927e2a
Andrew O'Mahony <andrew.omahony@saronic.com>
2025-07-17 16:07:52 -0500

    feat: Compiling against the proper RevPi kernel with the matching headers, Module.symvers, and .config

--- flake.nix
+++ flake.nix
@@ -16,12 +16,12 @@
         src = pkgs.fetchFromGitHub {
           owner = "RevolutionPi";
           repo = "linux";
-          rev = "revpi-6.1";
-          hash = "sha256-2CdHDlbnkFi+ehFNmiJhGy0T77xf2Fc2qS3jYFXwljA=";
+          rev = "revpi-6.6";
+          hash = "sha256-dj2OKvAX5HK4q1VL38gz0tmKGR5h8kmIMECDsOPz0Mo=";
         };
 
         nativeBuildInputs = with pkgs; [
-          gcc
+          gcc12
           bc
           flex
           bison
@@ -35,9 +35,33 @@
         buildPhase = ''
           # Prepare the kernel for module building
           make mrproper
-          make defconfig
+          cp ${revpi-kernel-headers}/.config .
           make modules_prepare
+          cp ${revpi-kernel-headers}/Module.symvers .
         '';
+        installPhase = ''
+          mkdir -p $out
+          cp -R . $out
+        '';
+      };
+
+      # Derivation to get Modules.symvers and .config for the RevPi that we are
+      # using.  It's RevPi Linux dependent; we are using 6.6 right now
+      revpi-kernel-headers = pkgs.stdenv.mkDerivation {
+        name = "revpi-kernel-headers";
+
+        src = pkgs.fetchFromGitHub {
+          owner = "saronic-technologies";
+          repo = "linux-headers-6.6.0-revpi6-rpi-v8";
+          rev = "ce6151e08909682291571f800104af92895aaeab";
+          hash = "sha256-G2QhZGtUaXbPS+Anex0Hv9Ogbx7kExXqYeFsDXBWbmU=";
+        };
+
+        # We need this for some reason, as Nix tries to build the headers otherwise
+        buildPhase = ''
+          echo "Dummy"
+        '';
+        # Copy all the header information into the $out directory
         installPhase = ''
           mkdir -p $out
           cp -R . $out
@@ -58,14 +82,14 @@
         src = ./.;
         
         nativeBuildInputs = [
-          build-module
           pkgs.xz
         ];
         
         # !!! I have no idea why I can't just build this using exactly what build-module
         # !!! does!  This is working for now, so I'm going to leave it as-is
         buildPhase = ''
-          build-module
+          # make KDIR=${revpi-kernel-prepared} clean
+          make KDIR=${revpi-kernel-prepared}
           # The default RevPi image has the module compressed, so we want to do the same
           xz piControl.ko
         '';
@@ -88,7 +112,7 @@
       devShells.${system}.default = pkgs.mkShell {
         packages = with pkgs; [
           gnumake
-          gcc
+          gcc12
           bc
           flex
           bison
@@ -98,19 +122,10 @@
           ncurses
           perl
           bear
-          build-module
         ];
         
         shellHook = ''
           # Create kernel source symlink in a known location
-          KERNEL_SRC="$HOME/.cache/revpi-kernel"
-          mkdir -p "$(dirname "$KERNEL_SRC")"
-          if [ ! -e "$KERNEL_SRC" ]; then
-            ln -sf ${revpi-kernel-prepared} "$KERNEL_SRC"
-          fi
-          
-          # Set KDIR for module compilation
-          export KDIR="$KERNEL_SRC"
           
           # compile_commands.json only works when made in this directory for some reason, which sucks
           make KDIR=${revpi-kernel-prepared} clean
@@ -120,7 +135,6 @@
           sed -i 's/"-mabi=lp64",//g' compile_commands.json
 
           echo "RevolutionPi kernel source symlinked to: $KERNEL_SRC"
-          echo "To build the module, run: build-module"
         '';
       };
     };
